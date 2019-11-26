1ba57b4474585f8f03a2f4315ca2a2ff5064c168
IgorSouza21 <igor11souza@gmail.com>
2019-11-26 07:33:14 -0300

    :) :D :) :D

--- Projeto.py
+++ Projeto.py
@@ -1,53 +1,55 @@
 import numpy as np
 import cv2
+import math
 # from principal import *
 
 
 def equaliza_histograma(img):
-    b, g, r = cv2.split(img)
-    red = cv2.equalizeHist(r)
-    green = cv2.equalizeHist(g)
-    blue = cv2.equalizeHist(b)
-    return cv2.merge((blue, green, red))
+    Y, Cr, Cb = cv2.split(img)
+    y = cv2.equalizeHist(Y)
+    cr = cv2.equalizeHist(Cr)
+    cb = cv2.equalizeHist(Cb)
+    return cv2.merge((y, cr, cb))
+
+
+def mostrarImagem(nomeDaTela, img):
+    cv2.imshow(nomeDaTela, img)
 
 
 def limiarizar(img):
-    ret, limiar = cv2.threshold(img, 100, 255, cv2.THRESH_TOZERO)
+    imagem = img.copy()
+    ret, limiar = cv2.threshold(imagem, 100, 255, cv2.THRESH_TOZERO)
     return limiar
 
 
-def binarizar_YCrCb(image, piso, teto):
-    #     img = equaliza_histograma(image)
-    #
-    #     piso = np.array([45, 35, 175], dtype=np.uint8)
-    #     teto = np.array([245, 255, 255], dtype=np.uint8)
-    #
-    #     mask = cv2.inRange(img, piso, teto)
-    #     mask = cv2.GaussianBlur(mask, (15, 15), 100)
-    #
-    #     return mask
+def binarizar_YCrCb(ent, piso, teto):
+    img = ent.copy()
     kernel = np.ones((3, 3), np.uint8)
-    img = limiarizar(image)
+
+    #apliico um filtro de mediana para remover a maior parte dos ruídos
+    img = cv2.medianBlur(img, 13)
+    #threshold
+    img = limiarizar(img)
+
+    # #Intervalo de cores em YCrCb para pele (foi calibrado no sangue, suor, lágrimas e tristeza)
+    # piso = np.array([0, 60, 75], dtype=np.uint8)
+    # teto = np.array([245, 145, 135], dtype=np.uint8)
 
     # Intervalo de cores em YCrCb para pele (foi calibrado no sangue, suor, lágrimas e tristeza)
-    # piso = np.array([0, 35, 100], dtype=np.uint8)
+    # piso = np.array([0, 60, 100], dtype=np.uint8)
     # teto = np.array([245, 175, 135], dtype=np.uint8)
 
     mask = cv2.inRange(img, piso, teto)
-    # isso aqui remove a maior parte dos ruidos
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_ERODE, kernel, iterations=2)
+    #isso aqui remove a maior parte dos ruidos
 
-    mask = cv2.GaussianBlur(mask, (3, 3), 5)
-    # mask = cv2.medianBlur(mask, 3)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=5)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel, iterations=3)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
 
-    return 255 - mask
+    # mask = cv2.GaussianBlur(mask, (3, 3), 5)
 
-
-def deixa_tudo_preto(image, rectangle):
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            pass
+    return 255-mask
 
 
 def show(image):
@@ -57,14 +59,58 @@ def show(image):
 
 def tentativa2(image, piso, teto):
     img = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-    img = binarizar_YCrCb(img, piso, teto)
-    mask = limiarizar(img)
-    kernel = np.array([[-1, -1, -1], [-1, 1, -1], [-1, -1, -1]])
-    mask = cv2.dilate(mask, kernel, iterations=7)
-    mask = cv2.erode(mask, kernel, iterations=1)
-    mask = cv2.Canny(mask, 100, 200)
+    mask = binarizar_YCrCb(img, piso, teto)
+    # mask = limiarizar(img)
+    # kernel = np.ones((3, 3), np.uint8)
+    # mask = cv2.dilate(mask, kernel, iterations=2)
+    # mask = cv2.erode(mask, kernel, iterations=1)
+    # mask = cv2.Canny(mask, 100, 200)
+
+    # ret, labels = cv2.connectedComponents(mask)
+    # # Associa os rótulos a cores
+    # label_hue = np.uint8(179 * labels / np.max(labels))
+    # blank_ch = 255 * np.ones_like(label_hue)
+    # labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+    #
+    # # HSV para BGR
+    # labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = max(contours, key=lambda x: cv2.contourArea(x))
+    cnt_poly = cv2.approxPolyDP(cnt, 3, True)
+    bound_rect = cv2.boundingRect(cnt_poly)
+    color = (255, 0, 0)
+    # cv2.drawContours(mask, cnt_poly, 0, color, cv2.FILLED)
+    cv2.rectangle(image, (int(bound_rect[0]), int(bound_rect[1])),
+                  (int(bound_rect[0] + bound_rect[2]), int(bound_rect[1] + bound_rect[3])), color, 2)
+
+    # cv2.floodFill(drawing, mask1, (int(boundRect[i][0]), int(boundRect[i][1])), (0, 0, 0))
+    # cv2.floodFill(drawing, mask1, (0, 0), (0, 0, 0))
+
+    # return labeled_img
+    return mask
+
+
+def tentativa3(frame):
+    kernel = np.ones((3, 3), np.uint8)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of skin color in HSV
+    lower_skin = np.array([0, 20, 70], dtype=np.uint8)
+    upper_skin = np.array([20, 255, 255], dtype=np.uint8)
+
+    # extract skin colur imagw
+    mask = cv2.inRange(hsv, lower_skin, upper_skin)
+
+    # extrapolate the hand to fill dark spots within
+    mask = cv2.dilate(mask, kernel, iterations=4)
+
+    # blur the image
+    mask = cv2.GaussianBlur(mask, (5, 5), 100)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = max(contours, key=lambda x: cv2.contourArea(x))
     contours_poly = [None] * len(contours)
     boundRect = [None] * len(contours)
     areas = [None] * len(contours)
@@ -73,49 +119,43 @@ def tentativa2(image, piso, teto):
         boundRect[i] = cv2.boundingRect(contours_poly[i])
         width = boundRect[i][2]
         height = boundRect[i][3]
-        areas[i] = width*height
+        areas[i] = width * height
     drawing = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
-    height, width = image.shape[:-1]
-    mask1 = np.zeros((height + 2, width + 2), dtype=np.uint8)
+    # height, width = image.shape[:-1]
+    # mask1 = np.zeros((height + 2, width + 2), dtype=np.uint8)
     for i in range(len(contours)):
         color = (255, 0, 0)
-        for con in contours_poly:
-            cv2.drawContours(drawing, [con], -1, (0, 0, 255), cv2.FILLED)
-
+        # for con in contours_poly:
+        #     cv2.drawContours(drawing, [con], -1, (0, 0, 255), cv2.FILLED)
+        cv2.drawContours(drawing, contours_poly, i, color, cv2.FILLED)
         if areas[i] > 75000:
-
-            cv2.rectangle(image, (int(boundRect[i][0]), int(boundRect[i][1])),
+            cv2.rectangle(frame, (int(boundRect[i][0]), int(boundRect[i][1])),
                           (int(boundRect[i][0] + boundRect[i][2]), int(boundRect[i][1] + boundRect[i][3])), color, 2)
             cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])),
                           (int(boundRect[i][0] + boundRect[i][2]), int(boundRect[i][1] + boundRect[i][3])), color, 2)
 
-     # cv2.floodFill(drawing, mask1, (int(boundRect[i][0]), int(boundRect[i][1])), (0, 0, 0))
-    cv2.floodFill(drawing, mask1, (0, 0), (0, 0, 0))
-
     return drawing
-
-
-camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-# Check if the webcam is opened correctly
-if not camera.isOpened():
-    raise IOError("Cannot open webcam")
-
-
-def captura():
-    a, img = camera.read()
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb) # conversão para YCrCb
-    out = tentativa2(img)
-
-    return img, out
-
-
-def main():
-    while True:
-        img, out = captura()
-        cv2.imshow("olhaso", img)
-        cv2.imshow("outro", out)
-        if cv2.waitKey(1) == 27:
-            break
+# camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# # Check if the webcam is opened correctly
+# if not camera.isOpened():
+#     raise IOError("Cannot open webcam")
+#
+#
+# def captura():
+#     a, img = camera.read()
+#     # img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb) # conversão para YCrCb
+#     out = tentativa2(img)
+#
+#     return img, out
+#
+#
+# def main():
+#     while True:
+#         img, out = captura()
+#         cv2.imshow("olhaso", img)
+#         cv2.imshow("outro", out)
+#         if cv2.waitKey(1) == 27:
+#             break
 
 
 # main()
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
-import time
 import cv2
 import numpy as np
 import Projeto
@@ -12,25 +11,24 @@ class CameraClick(BoxLayout):
     min = 0
     max = 255
     minY = 0
-    maxY = 245
-    minCr = 35
-    maxCr = 175
+    minCr = 60
     minCb = 100
+    maxY = 245
+    maxCr = 175
     maxCb = 135
 
     def __init__(self, **kwargs):
         super(CameraClick, self).__init__(**kwargs)
         self.capture = cv2.VideoCapture(0)
-        # ret, frame = self.capture.read()
         Clock.schedule_interval(self.atualizaImagem,
                                 1.0 / 30.0)
 
-    def bufferiza(self, frame):
+    def bufferiza(self, frame, color):
         buf1 = cv2.flip(frame, 0)  # inverte para não ficar de cabeça para baixo
         buf = buf1.tostring()  # converte em textura
 
-        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt=color)
+        texture1.blit_buffer(buf, colorfmt=color, bufferfmt='ubyte')
         return texture1
 
     def atualizaImagem(self, dt):
@@ -39,20 +37,14 @@ class CameraClick(BoxLayout):
         teto = np.array([self.ids['maxY'].value, self.ids['maxCr'].value, self.ids['maxCb'].value], dtype=np.uint8)
         frame2 = Projeto.tentativa2(frame, piso, teto)
 
-        texture1 = self.bufferiza(frame)
-        texture2 = self.bufferiza(frame2)
+        # frame2 = Projeto.tentativa3(frame)
+
+        texture1 = self.bufferiza(frame, 'bgr')
+        texture2 = self.bufferiza(frame2, 'luminance')
 
         self.ids['cam1'].texture = texture1  # apresenta a imagem
         self.ids['cam2'].texture = texture2  # apresenta a imagem
 
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['image']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
 
 class CameraApp(App):
 
